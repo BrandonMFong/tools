9d8fd6afd752ca0162b9ae5f49bf59885061852b
brandonmfong <brandonmfong@no-email.org>
2022-06-07 14:03:45 -0700

    Created date

--- src/fsinfo/main.c
+++ src/fsinfo/main.c
@@ -10,6 +10,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
 
 const char SCRIPT_ARG[PATH_MAX];
 
@@ -46,14 +48,29 @@ int main(int argc, char * argv[]) {
 }
 
 int PrintInfo(const char * path) {
+	const char * dateFormat = "%02d:%02d:%02d%s %02d/%02d/%d";
 	char absPath[PATH_MAX];
 	int result = 0;
+	char creation[strlen(dateFormat)], lastModified[strlen(dateFormat)];
+	struct stat st;
 
 	realpath(path, absPath);
-	
+	stat(path, &st);
+	struct tm ct = *localtime(&st.st_ctime);
+
+	sprintf(creation, dateFormat,
+		ct.tm_hour > 12 ? ct.tm_hour - 12 : ct.tm_hour,
+		ct.tm_min,
+		ct.tm_sec,
+		ct.tm_hour < 12 ? " AM" : " PM",
+		ct.tm_mon + 1,
+		ct.tm_mday,
+		ct.tm_year + 1900
+	);
+
 	printf(	"Full path: %s\n"
 		"Created: %s\n"
-		"\n", absPath, "tmp");
+		"\n", absPath, creation);
 	return result;
 }
 
