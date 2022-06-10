db274d04f8daab8d3db26ceeb54d1ea27a2ab8e4
brandonmfong <brandonmfong@no-email.org>
2022-06-10 07:31:17 -0700

    Having help

--- src/getcount/main.c
+++ src/getcount/main.c
@@ -5,8 +5,24 @@
 
 #include "../cutils/utilities.h"
 #include <stdio.h>
+#include <string.h>
+
+char TOOL_ARG[PATH_MAX];
+
+void Help() {
+	printf("usage: %s <path>\n", TOOL_ARG);
+}
 
 int main(int argc, char * argv[]) {
+	int result = 0;
 	
-	return 0;
+	strcpy(TOOL_ARG, argv[0]);
+
+	if (argc != 2) {
+		Help();
+	} else {
+
+	}
+
+	return result;
 }
