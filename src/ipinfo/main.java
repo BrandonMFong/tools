f3cd1650289238453b95eb119db579df6f7eb42b
brandonmfong <brandonmfong@no-email.org>
2022-06-17 14:29:17 -0700

    Can get mac address

---
+++ src/ipinfo/main.java
@@ -0,0 +1,75 @@
+// Your First Program
+/**
+ * author: Brando
+ * date: 6/17/22
+ */
+
+import java.util.Enumeration;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.lang.Throwable;
+import java.util.NoSuchElementException;
+
+class IPInfo {
+	static void PrintException(Throwable e) {
+		System.out.println("[ " + e.toString() + " ]");
+		System.out.println("Message: " + e.getMessage());
+		System.out.println("Stack trace:");
+		e.printStackTrace();
+	}
+
+	public static void main(String[] args) {
+		int result = 0;
+		Enumeration<NetworkInterface> niEnum = null;
+
+		try {
+			niEnum = NetworkInterface.getNetworkInterfaces();
+
+			if (niEnum == null) {
+				result = 1;
+				System.out.println("Could not get the network interfaces");
+			} else {
+				while (niEnum.hasMoreElements() && (result == 0)) {
+					NetworkInterface ni = null;
+					byte[] mac = null;
+
+					try {
+						ni = niEnum.nextElement();
+						if (ni == null) {
+							result = 1;
+							System.out.println("Could not get the next element");
+						}
+					} catch (NoSuchElementException e) {
+						result = 1;
+						PrintException(e);
+					}
+
+					if (result == 0) {
+						mac = ni.getHardwareAddress();
+						if (mac == null) {
+							result = 1;
+							System.out.println("Could not get hardware address");
+						}
+					}
+
+					if (result == 0) {
+						String[] hexadecimal = new String[mac.length];
+						for (int i = 0; i < mac.length; i++) {
+						hexadecimal[i] = String.format("%02X", mac[i]);
+						}
+						String macAddress = String.join("-", hexadecimal);
+						System.out.println(macAddress);
+					}
+				}
+			}
+		} catch (SocketException e) {
+			result = 1;
+			PrintException(e);
+		}
+
+		System.exit(result);
+	}
+}
+
