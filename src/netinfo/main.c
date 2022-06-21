d3b3bda559ce70aaf3ddba038c6e5df3a5c5c8a0
brandonmfong <brandonmfong@no-email.org>
2022-06-21 12:21:00 -0700

    Successfully getting address

--- src/netinfo/main.c
+++ src/netinfo/main.c
@@ -9,6 +9,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <stdint.h>
+#include <string.h>
+#include <linux/if_packet.h>
+#include <stdlib.h>
 
 #ifdef LINUX
 #define AF_HW AF_PACKET
@@ -17,6 +21,35 @@
 #else 
 #error Unknown OS
 #endif
+
+char * CopyMacAddress(struct sockaddr * sa, int * err) {
+	char * result = 0;
+	int error = 0;
+	char buf[20]; // buffer for the mac address string
+
+	memset(&buf[0], 0, 20);
+
+#ifdef LINUX
+	struct sockaddr_ll * saddr = (struct sockaddr_ll *) sa;
+
+	int l = 0;
+	for (int i = 0; i < 6; i++) {
+		l += sprintf(buf+l, "%02X%s", saddr->sll_addr[i], i < 5 ? ":" : "");
+	}
+#elif OSX
+
+#endif
+
+	if (!error) {
+		result = CopyString(buf, &error);
+	}
+
+	if (err != 0) {
+		*err = error;
+	}
+
+	return result;
+}
 
 int main() {
 	struct ifaddrs *addrs,*tmp;
@@ -37,11 +70,9 @@ int main() {
 				printf("%s\n", addr);
 			} else if (tmp->ifa_addr->sa_family == AF_HW) {
 				printf("%d: %s %d\n", tmp->ifa_addr->sa_family, tmp->ifa_name, sizeof(tmp->ifa_addr->sa_data));
-				for (int i = 0; i < sizeof(tmp->ifa_addr->sa_data); i++) {
-					printf("%02x ", (unsigned char) tmp->ifa_addr->sa_data[i]);
-				}
-
-				printf("\n");
+				char * mac = CopyMacAddress(tmp->ifa_addr, 0);
+				printf("%s\n", mac);
+				free(mac);
 			}
 		}
 
