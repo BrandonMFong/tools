781c5dade420827180027214be77978c4c875808
brandonmfong <brandonmfong@no-email.org>
2022-06-21 10:41:17 -0700

    Successfully getting ip address

--- src/netinfo/main.c
+++ src/netinfo/main.c
@@ -8,22 +8,24 @@
 #include <ifaddrs.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 int main() {
 	struct ifaddrs *addrs,*tmp;
+	struct sockaddr_in *sa;
 
 	getifaddrs(&addrs);
 	tmp = addrs;
 
 	while (tmp)
 	{
+		// https://stackoverflow.com/questions/4139405/how-can-i-get-to-know-the-ip-address-for-interfaces-in-c
 		if (tmp->ifa_addr && (tmp->ifa_addr->sa_family == AF_INET)) {
 			printf("%d: %s %d\n", tmp->ifa_addr->sa_family, tmp->ifa_name, sizeof(tmp->ifa_addr->sa_data));
 
-			for (int i = 0; i < sizeof(tmp->ifa_addr->sa_data); i++) {
-				printf("%u ", (unsigned char) tmp->ifa_addr->sa_data[i]);
-			}
-			printf("\n");
+			sa = (struct sockaddr_in *) tmp->ifa_addr;
+			char * addr = inet_ntoa(sa->sin_addr);
+			printf("%s\n", addr);
 		}
 
 		tmp = tmp->ifa_next;
