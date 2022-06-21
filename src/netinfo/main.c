8d14e57938901991a7b45782039dcd557c3fef65
brandonmfong <brandonmfong@no-email.org>
2022-06-21 08:12:02 -0700

    Getting the correct interface

--- src/netinfo/main.c
+++ src/netinfo/main.c
@@ -5,91 +5,25 @@
 
 #include <clib/clib.h>
 #include <stdio.h>
-#include <string.h>
+#include <ifaddrs.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
 
-int main(void) {
-	int result = 0;
-	char buf[8192] = {0};
-	struct ifconf ifc = {0};
-	struct ifreq *ifr = NULL;
-	int sock = 0;
-	int nInterfaces = 0;
-	int i = 0;
-	char ip[INET6_ADDRSTRLEN] = {0};
-	char macp[19];
-	struct ifreq *item;
-	struct sockaddr *addr;
+int main() {
+struct ifaddrs *addrs,*tmp;
 
-	/* Get a socket handle. */
-	sock = socket(PF_INET, SOCK_DGRAM, 0);
-	if(sock < 0) {
-		result = 1;
-		Error("Socket error");
-	}
+getifaddrs(&addrs);
+tmp = addrs;
 
-	if (!result) {
-		/* Query available interfaces. */
-		ifc.ifc_len = sizeof(buf);
-		ifc.ifc_buf = buf;
-		if(ioctl(sock, SIOCGIFCONF, &ifc) < 0) {
-			result = 1;
-			Error("ioctl error");
-		}
-	}
+while (tmp)
+{
+     if (tmp->ifa_addr && (tmp->ifa_addr->sa_family == AF_INET))
+        printf("%d: %s\n", tmp->ifa_addr->sa_family, tmp->ifa_name);
 
-	if (!result) {
-	 	/* Iterate through the list of interfaces. */
-	 	ifr = ifc.ifc_req;
-		nInterfaces = ifc.ifc_len / sizeof(struct ifreq);
+    tmp = tmp->ifa_next;
+}
 
-		printf("inter\tmac\t\t\tip\n");
-		for(i = 0; (i < nInterfaces) && !result; i++) 
-		{
-			item = &ifr[i];
-
-			addr = &(item->ifr_addr);
-
-			/* Get the IP address*/
-			if(ioctl(sock, SIOCGIFADDR, item) < 0) {
-				Error("ioctl error with ip addr");
-				result = 1;
-			}
-
-			if (!result) {
-				if (inet_ntop(AF_INET, &(((struct sockaddr_in *)addr)->sin_addr), ip, sizeof ip) == NULL) {
-					result = 1;
-					Error("Could not get ip address");
-				}
-			}
-
-			if (!result) {
-				/* Get the MAC address */
-				if(ioctl(sock, SIOCGIFHWADDR, item) < 0) {
-				    result = 1;
-				    Error("ioctl error with mac addr");
-				}
-			}
-
-			/* display result */
-			if (!result) {
-				sprintf(macp, "%02x:%02x:%02x:%02x:%02x:%02x", 
-				(unsigned char)item->ifr_hwaddr.sa_data[0],
-				(unsigned char)item->ifr_hwaddr.sa_data[1],
-				(unsigned char)item->ifr_hwaddr.sa_data[2],
-				(unsigned char)item->ifr_hwaddr.sa_data[3],
-				(unsigned char)item->ifr_hwaddr.sa_data[4],
-				(unsigned char)item->ifr_hwaddr.sa_data[5]);
-
-				printf("%s\t%s\t%s\n", item->ifr_name, macp, ip);
-			}
-		}
-	}
-
-	return result;
+freeifaddrs(addrs);
+	return 0;
 }
 
