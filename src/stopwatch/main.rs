a921714b9d26b59c4622fdc05dd93c1db7af47d1
Brando <fong.m.brandon97@gmail.com>
2023-08-21 14:26:09 -0700

    stopwatch

--- src/stopwatch/main.rs
+++ src/stopwatch/main.rs
@@ -9,6 +9,7 @@ use std::path::Path;
 use std::{thread, time};
 
 const ARGUMENTS_HELP: &str = "-h";
+const BRIEF_DESCRIPTION_HELP: &str = "--brief-description";
 
 fn help(args: Vec<String>) {
     let tool_path = Path::new(&args[0]);
@@ -22,11 +23,17 @@ fn help(args: Vec<String>) {
     println!("  laps, simply press enter.  To cancel, ctrl+c.");
 }
 
+fn brief_description() {
+    println!("simple stop watch");
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.contains(&ARGUMENTS_HELP.to_string()) {
         help(args);
+    } else if args.contains(&BRIEF_DESCRIPTION_HELP.to_string()) {
+        brief_description();
     } else {
         let ten_millis = time::Duration::from_millis(10);
         let now = time::Instant::now();
