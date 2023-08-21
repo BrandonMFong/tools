978299d7a3cc7a36eac922cf696fa78b61b03b7a
Brando <fong.m.brandon97@gmail.com>
2023-08-21 14:16:40 -0700

    organize

--- src/listtools/script.sh
+++ src/listtools/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tools=(check cpy getcount getpath getsize ip4domain mytime num2bin num2hex);
+tools=(check cpy getcount getpath getsize ip4domain mytime num2bin num2hex organize);
 for tool in "${tools[@]}"
 do
 	printf "$tool: ";
