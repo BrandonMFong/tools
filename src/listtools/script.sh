ff47cc8a383002499ddf6ccfbc5da1fecc82bc91
Brando <fong.m.brandon97@gmail.com>
2023-08-21 14:12:39 -0700

    num2hex

--- src/listtools/script.sh
+++ src/listtools/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tools=(check cpy getcount getpath getsize ip4domain mytime num2bin);
+tools=(check cpy getcount getpath getsize ip4domain mytime num2bin num2hex);
 for tool in "${tools[@]}"
 do
 	printf "$tool: ";
--- src/num2hex/main.rs
+++ src/num2hex/main.rs
@@ -9,9 +9,14 @@ use bflib::core::string::string_is_numeric;
 use std::env;
 
 const ARGUMENT_HELP: &str = "-h";
+const ARGUMENT_BRIEF_DESCRIPTION: &str = "--brief-description";
 
 fn help() {
     println!("usage: {} <value> {}", env::args().nth(0).unwrap(), ARGUMENT_HELP);
+}
+
+fn brief_description() {
+    println!("converts number to hex");
 }
 
 fn main() {
@@ -19,10 +24,13 @@ fn main() {
     let mut get_help: bool = false;
     let mut input_value: i32 = 0;
     let mut i: i8 = 0;
+    let mut briefly_describe: bool = false;
 
     for argument in env::args() {
         if i > 0 {
-            if argument == ARGUMENT_HELP {
+            if argument == ARGUMENT_BRIEF_DESCRIPTION {
+                briefly_describe = true
+            } else if argument == ARGUMENT_HELP {
                 get_help = true
             } else {
                 if string_is_numeric(argument.clone()) {
@@ -42,6 +50,8 @@ fn main() {
     if error == 0 {
         if get_help || env::args().count() == 1 {
             help();
+        } else if briefly_describe {
+            brief_description();
         } else {
             println!("{:#x}", input_value);
         }
