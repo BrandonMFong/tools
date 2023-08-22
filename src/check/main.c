14e771dde3052a7ad257e0e6738e5eeb2b5ba79b
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2023-08-22 10:25:12 -0700

    [check] adding verbose mode (#8)

--- src/check/main.c
+++ src/check/main.c
@@ -25,28 +25,43 @@
 #define kArgumentsSHA512 "sha512"
 #define kArgumentsBrieflyDescribeTool "--brief-description"
 
-#define kArgumentsExpected "-e"
+#define kArgumentsExpected "-expect"
+
+/**
+ * Notes
+ *
+ * This allows the pthreads to print to stdout. The should 
+ * be revisited if there are print operations in other threads
+ *
+ */
+#define kArgumentFlagVerbose "v"
 
 // Types
 typedef struct {
 	int fd; // file descriptor
-	off_t * currSeek;
+	unsigned long long * currSeek;
 	BFChecksumTools * checksum;
 	pthread_mutex_t * mutexRead;
 	pthread_mutex_t * mutexHash;
 	int error;
+	bool verbose;
+	unsigned long long fileSize;
 } ChecksumTools;
 
 // Global variables
 char filePath[PATH_MAX];
-unsigned long long fileSize = 0;
 
 // Constants
 const int kThreadCount = 2;
 size_t kBufferSizeRead = 4096 * 4096;
 
 void help(int argc, char ** argv) {
-	printf("usage: %s <checksum type> [ %s <expected hash> ] <file path>\n", basename(argv[0]), kArgumentsExpected);
+	printf("usage: %s [ -<flags> ] <checksum type> [ %s <expected hash> ] <file path>\n", 
+			basename(argv[0]), 
+			kArgumentsExpected);
+
+	printf("\nFlags:\n");
+	printf("  [ %s ] : verbose. Prints out calculation process\n", kArgumentFlagVerbose);
 
 	printf("\n");
 	printf("Checksum types:\n");
@@ -61,34 +76,58 @@ void BriefDescription() {
 }
 
 void * PThreadReadAndHash(void * _tools);
-int CalculateChecksumForPath(const char * filePath, BFChecksumType checksumType, const char * expected);
+int CalculateChecksumForPath(const char * filePath, BFChecksumType checksumType, const char * expected, bool verbose);
 
 int main(int argc, char ** argv) {
 	int error = 0;
 	BFChecksumType checksumType = kBFChecksumTypeUnknown;
+	int argIndex = 1;
 	bool okayToContinue = true;
+	bool verbose = false;
 
-	if (argc < 3) {
+	if (argc < 2) {
+		help(argc, argv);
+		okayToContinue = false;
+	} else if (argc < 3) {
 		if (!strcmp(argv[1], kArgumentsBrieflyDescribeTool)) {
 			BriefDescription();
 			okayToContinue = false;
 		} else {
+			BFErrorPrint("Unknown arg: %s", argv[1]);
 			help(argc, argv);
 			okayToContinue = false;
 		}
 	} else {
-		if (!strcmp(argv[1], kArgumentsMD5)) {
+		if (argv[argIndex][0] == '-' && argv[argIndex][1] != '-') {
+			char flags[2 << 4];
+			strcpy(flags, argv[argIndex]);
+			for (int i = 1; i < strlen(flags); i++) {
+				switch (flags[i]) {
+					case 'v':
+						verbose = true;
+						break;
+					default:
+						break;
+				}
+			}
+			
+			argIndex++;
+		}
+
+		if (!strcmp(argv[argIndex], kArgumentsMD5)) {
 			checksumType = kBFChecksumTypeMD5;
-		} else if (!strcmp(argv[1], kArgumentsSHA1)) {
+		} else if (!strcmp(argv[argIndex], kArgumentsSHA1)) {
 			checksumType = kBFChecksumTypeSHA1;
-		} else if (!strcmp(argv[1], kArgumentsSHA256)) {
+		} else if (!strcmp(argv[argIndex], kArgumentsSHA256)) {
 			checksumType = kBFChecksumTypeSHA256;
-		} else if (!strcmp(argv[1], kArgumentsSHA512)) {
+		} else if (!strcmp(argv[argIndex], kArgumentsSHA512)) {
 			checksumType = kBFChecksumTypeSHA512;
 		} else {
 			error = 8;
 			BFErrorPrint("Please provide a checksum type");
 		}
+
+		argIndex++;
 	}
 
 	if (okayToContinue) {
@@ -113,17 +152,18 @@ int main(int argc, char ** argv) {
 		}
 
 		if (error == 0) {
-			error = CalculateChecksumForPath(filePath, checksumType, expected);
+			error = CalculateChecksumForPath(filePath, checksumType, expected, verbose);
 		}
 	}
 
 	return error;
 }
 
-int CalculateChecksumForPath(const char * filePath, BFChecksumType checksumType, const char * expected) {
+int CalculateChecksumForPath(const char * filePath, BFChecksumType checksumType, const char * expected, bool verbose) {
 	int error = 0;
 	BFChecksumTools ctools = {0};
 	pthread_mutex_t mutexRead, mutexHash;
+	unsigned long long fileSize = 0;
 
 	// Init checksum tools
 	error = BFChecksumCreate(&ctools, checksumType);
@@ -142,7 +182,7 @@ int CalculateChecksumForPath(const char * filePath, BFChecksumType checksumType,
 		fileSize = BFFileSystemFileGetSizeUsed(filePath, 0, &error);
 	}
 
-	off_t currSeek = 0;
+	unsigned long long currSeek = 0;
 	int i = 0;
 	pthread_t threads[kThreadCount];
 	ChecksumTools checksumTools[kThreadCount];
@@ -155,6 +195,8 @@ int CalculateChecksumForPath(const char * filePath, BFChecksumType checksumType,
 			checksumTools[i].mutexRead = &mutexRead;
 			checksumTools[i].mutexHash = &mutexHash;
 			checksumTools[i].checksum = &ctools;
+			checksumTools[i].verbose = verbose;
+			checksumTools[i].fileSize = fileSize;
 		}
 
 		if ((error == 0) && pthread_create(&threads[i], 0, PThreadReadAndHash, &checksumTools[i])) {
@@ -227,7 +269,7 @@ void * PThreadReadAndHash(void * _tools) {
 	
 		// READ
 
-		size_t upcoming = fileSize - *tools->currSeek;
+		size_t upcoming = tools->fileSize - *tools->currSeek;
 		if (upcoming == 0) { // no more bytes to read
 			finished = true;
 		} else if (upcoming < bufferSize) {
@@ -254,8 +296,19 @@ void * PThreadReadAndHash(void * _tools) {
 
 		// HASH
 		
-		if (!finished && (tools->error == 0)) {
-			tools->error = BFChecksumUpdate(tools->checksum, buf, bufferSize);
+		if (!finished) {
+			if (tools->error == 0) {
+				tools->error = BFChecksumUpdate(tools->checksum, buf, bufferSize);
+			}
+
+			// Run verbose option
+			if (tools->error == 0 && tools->verbose) {
+				printf("\rProgress: %.2f%%",
+						((float) *tools->currSeek / (float) tools->fileSize) * 100);
+			}
+		} else {
+			// Delete line when we are finished
+			if (tools->error == 0 && tools->verbose) printf("\r");
 		}
 		
 		pthread_mutex_unlock(tools->mutexHash);
